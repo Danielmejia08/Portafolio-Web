c6aeb602db4606dea7d812017d05b7066d6a5e47
Cartagena2001 <43362524+Cartagena2001@users.noreply.github.com>
2020-11-16 15:27:19 -0600

    Slider

--- js/script.js
+++ js/script.js
@@ -5,7 +5,29 @@ $(document).ready(function(){
     });
 });
 
-$('.owl-carousel').owlCarousel({
+
+$('.sliderTrabajos').owlCarousel({
+    loop:true,
+    nav:true,
+    margin:50,
+    autoWidth:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+
+$('.publicidadSlider').owlCarousel({
     loop:false,
     margin:10,
     nav:true,
@@ -20,4 +42,4 @@ $('.owl-carousel').owlCarousel({
             items:5
         }
     }
-})
+});
