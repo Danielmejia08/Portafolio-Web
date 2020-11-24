36794fcb3bebae4ba3cc619ea28d8b62b8c5ca9e
Cartagena2001 <43362524+Cartagena2001@users.noreply.github.com>
2020-11-24 00:18:45 -0600

    hola

--- enviar.php
+++ enviar.php
@@ -1,24 +1,31 @@
 <?php
-$name = $_POST['name'];
-$mail = $_POST['mail'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
+$destino = "correo@dominio.com";
+$email = $_POST["mail"];
+$numero = $_POST["phone"];
+$mensaje = $_POST["message"];
+$contenido = "Nombre: " . $nombre . "\nCorreo: " . $email . "\nNúmero: " . $numero . "\nAsunto: " . $mensaje;
+@mail($destino, "Contacto", $contenido);
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+// $name = $_POST['name'];
+// $mail = $_POST['mail'];
+// $phone = $_POST['phone'];
+// $message = $_POST['message'];
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message .= "Su e-mail es: " . $mail . " \r\n";
-$message .= "Teléfono de contacto: " . $phone . " \r\n";
-$message .= "Mensaje: " . $_POST['message'] . " \r\n";
-$message .= "Enviado el: " . date('d/m/Y', time());
+// $header = 'From: ' . $mail . " \r\n";
+// $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+// $header .= "Mime-Version: 1.0 \r\n";
+// $header .= "Content-Type: text/plain";
 
-$para = 'guillermohdloco@gmail.com';
-$asunto = 'Mensaje de Usuario';
+// $message = "Este mensaje fue enviado por: " . $name . " \r\n";
+// $message .= "Su e-mail es: " . $mail . " \r\n";
+// $message .= "Teléfono de contacto: " . $phone . " \r\n";
+// $message .= "Mensaje: " . $_POST['message'] . " \r\n";
+// $message .= "Enviado el: " . date('d/m/Y', time());
 
-mail($para, $asunto, utf8_decode($message), $header);
+// $para = 'salvadorcartajena@hotmail.com';
+// $asunto = 'Mensaje de Usuario';
 
-header("Location:index.html");
+// mail($para, $asunto, utf8_decode($message), $header);
+
+// header("Location:index.html");
 ?>
