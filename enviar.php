cc639f3d1a83351ba88543d406179e9fe5065b37
Cartagena2001 <43362524+Cartagena2001@users.noreply.github.com>
2020-11-24 23:17:06 -0600

    dwa

--- enviar.php
+++ enviar.php
@@ -9,20 +9,4 @@ $contenido = "Nombre: " . $nombre . "\nCorreo: " . $email . "\nTelefono: " . $te
 mail($destino,"Contacto",$contenido);
 header("Location:index.html");
 
-
-// if (isset($_POST['enviar'])){
-//     if(!empty($_POST['name']) && !empty($_POST['mail']) && !empty($_POST['phone']) && !empty($_POST['message'])){
-//         $name = $_POST['name'];
-//         $email = $_POST['mail'];
-//         $phone = $_POST['phone'];
-//         $message = $_POST['message'];
-//         $header  = "From: salvadorcartajena@hotmail.com". "\r\n";
-//         $header.= "Reply-to: salvadorcartajena@hotmail.com". " \r\n";
-//         $header.= "X-Mailer: PHP/" . phpversion();
-//         $mail = mail($email, $name, $message, $header);
-//         if($mail){
-//             echo "<h4> Mail enviado con exito </h4>";
-//         }
-//     }
-// }
 ?>
